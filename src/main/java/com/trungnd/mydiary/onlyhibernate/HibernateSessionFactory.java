226543e5d67b2d981e144a1add9e1c26a53f09ce
trungnguyen19 <nongdan2m@gmail.com>
2014-08-20 11:23:49 +0700

    config xml (jpa is more flexible, can not change location for hibernate Persisce object)

---
+++ src/main/java/com/trungnd/mydiary/onlyhibernate/HibernateSessionFactory.java
@@ -0,0 +1,60 @@
+package com.trungnd.mydiary.onlyhibernate;
+
+import java.util.List;
+import java.util.Iterator;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
+import com.trungnd.mydiary.model.Record;
+
+public class HibernateSessionFactory {
+	private static SessionFactory factory;
+	private static ServiceRegistry serviceRegistry;
+
+	public static void main(String[] args) {
+		try {
+			Configuration configuration = new Configuration();
+			configuration.configure()
+					.setProperty("hibernate.show_sql", "false");
+			serviceRegistry = new ServiceRegistryBuilder().applySettings(
+					configuration.getProperties()).buildServiceRegistry();
+			factory = configuration.buildSessionFactory(serviceRegistry);
+		} catch (Throwable ex) {
+			System.err.println("Failed to create sessionFactory object." + ex);
+			throw new ExceptionInInitializerError(ex);
+		}
+		System.out.println("**Example : Hibernate 4 SessionFactory**");
+		System.out.println("----------------------------------------");
+		Session session = factory.openSession();
+		Transaction tx = null;
+		try {
+			tx = session.beginTransaction();
+			List records = session.createQuery("FROM RECORDS").list();
+			for (Iterator iterator = records.iterator(); iterator.hasNext();) {
+				Record record = (Record) iterator.next();
+				System.out.println("Id: " + record.getId());
+				System.out.println("CashIn: " + record.getCashIn());
+				System.out.println("CashOut: " + record.getCashOut());
+				System.out.println("Label: " + record.getLabel());
+				System.out.println("Note: " + record.getNote());
+				System.out.println("Day: " + record.getDay());
+				System.out.println("IsTransaction: "
+						+ record.getIsTransaction());
+				System.out.println("----------------------------------------");
+			}
+			tx.commit();
+		} catch (HibernateException e) {
+			if (tx != null)
+				tx.rollback();
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+	}
+}
