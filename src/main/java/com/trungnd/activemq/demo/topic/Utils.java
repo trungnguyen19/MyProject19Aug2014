06aee728f7a85d50d6beef18c456ce5318d0d7be
trungnguyen19 <nongdan2m@gmail.com>
2014-08-27 20:21:20 +0700

    update topic

---
+++ src/main/java/com/trungnd/activemq/demo/topic/Utils.java
@@ -0,0 +1,7 @@
+package com.trungnd.activemq.demo.topic;
+
+public class Utils {
+	public static String destinationName = "ABC1";
+	//public static String connectionUrl = "tcp://172.21.2.205:61616";
+	public static String connectionUrl = "tcp://localhost:61616";
+}
